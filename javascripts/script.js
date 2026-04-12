a3d7663ef6ffd381b18dbc22b19aa588c296f805
Joy Adelegan <96977916+AdeleganJoy@users.noreply.github.com>
2026-04-12 04:17:19 +0000

    Added the html javascript and css files for the login page

---
+++ javascripts/script.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+
+const form = document.querySelector('form');
+const error_message = document.getElementById("error-message");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxdNdMGR9NRqTWvYDY7W0qGljygfYV-Ns",
+  authDomain: "af-booking.firebaseapp.com",
+  databaseURL: "https://af-booking-default-rtdb.firebaseio.com",
+  projectId: "af-booking",
+  storageBucket: "af-booking.firebasestorage.app",
+  messagingSenderId: "626967665083",
+  appId: "1:626967665083:web:fde45e23f44b1fbc5ecc9e",
+  measurementId: "G-C8G2VFBX03"
+};
+
+// Initialize app
+const app = initializeApp(firebaseConfig);
+
+// Initialize auth
+const auth = getAuth(app);
+
+form.addEventListener('submit', async (e) => {
+	e.preventDefault(); 
+	error_message.textContent = "";
+	const username = document.querySelector("#username").value;
+	const password = document.querySelector("#password").value;
+
+  //Check if username and password valid
+  try {
+  const credential = await signInWithEmailAndPassword(auth, username, password);
+  const user = credential.user;
+
+  const token = await user.getIdToken();
+
+  console.log("Token:", token);
+  } 
+  catch (error) {
+    error_message.textContent = "Please enter a valid email address.";
+  }
+});
